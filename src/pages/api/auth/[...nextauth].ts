7205efec48d09d7fa178b4ec5d274a0cb03ddc09
Mxk01 <RLuntraru22@gmail.com>
2023-06-10 16:19:29 +0300

    =

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import DiscordProvider from "next-auth/providers/discord"
 import TwitterProvider from "next-auth/providers/twitter"
+import Auth0Provider from "next-auth/providers/auth0";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -19,6 +20,11 @@ export const authOptions = {
         clientId: process.env.DISCORD_CLIENT_ID as string,
         clientSecret: process.env.DISCORD_CLIENT_SECRET as string
       }),
+      Auth0Provider({
+        clientId: process.env.AUTH0_CLIENT_ID as string,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+        issuer: process.env.AUTH0_ISSUER as string
+      })
     //   TwitterProvider({
     //     clientId: process.env.TWITTER_CLIENT_ID as string,
     //     clientSecret: process.env.TWITTER_CLIENT_SECRET as string
